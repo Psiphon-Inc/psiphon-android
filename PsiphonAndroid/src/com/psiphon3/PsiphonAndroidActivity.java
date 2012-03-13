99ae31cdea3727989a0a41fb97d6d03b83d4b10b
Adam Pritchard <pritchard.adam@gmail.com>
2012-03-13 13:22:36 -0400

    Added Android folder with hello-world code.

---
+++ PsiphonAndroid/src/com/psiphon3/PsiphonAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.psiphon3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PsiphonAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
