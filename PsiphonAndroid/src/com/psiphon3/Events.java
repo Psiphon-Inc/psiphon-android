9fb0185106a15374971937bad92b858c9f614575
Adam Pritchard <pritchard.adam@gmail.com>
2012-04-27 15:52:24 -0400

    Fixed line endings and tabs

--- PsiphonAndroid/src/com/psiphon3/Events.java
+++ PsiphonAndroid/src/com/psiphon3/Events.java
@@ -29,8 +29,8 @@ public class Events
 {
     static public void appendStatusMessage(Context context, String message, int messageClass)
     {
-    	// Local broadcast to any existing status screen
-    	LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+        // Local broadcast to any existing status screen
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
         Intent intent = new Intent(StatusActivity.ADD_MESSAGE);
         intent.putExtra(StatusActivity.ADD_MESSAGE_TEXT, message);
         intent.putExtra(StatusActivity.ADD_MESSAGE_CLASS, messageClass);
@@ -39,46 +39,46 @@ public class Events
 
     static public void displayStatus(Context context)
     {
-    	// Simply display the status screen
-    	Intent intent = new Intent(
-    			"ACTION_VIEW",
-    			null,
-    			context,
-    			com.psiphon3.StatusActivity.class);
-    	intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        context.startActivity(intent);    	
+        // Simply display the status screen
+        Intent intent = new Intent(
+                "ACTION_VIEW",
+                null,
+                context,
+                com.psiphon3.StatusActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(intent);        
     }
     
     static public void displayBrowser(Context context)
     {
-    	displayBrowser(context, null);
+        displayBrowser(context, null);
     }
 
     static public void displayBrowser(Context context, Uri uri)
-    {    	
-    	Intent intent = new Intent(
-    			"ACTION_VIEW",
-    			uri,
-    			context,
-    			org.zirco.ui.activities.MainActivity.class);
+    {        
+        Intent intent = new Intent(
+                "ACTION_VIEW",
+                uri,
+                context,
+                org.zirco.ui.activities.MainActivity.class);
 
-    	// This intent displays the Zirco browser.
-    	// We use "extras" to communicate Psiphon settings to Zirco, which
-    	// is packaged as an independent component (so it's can't access,
-    	// e.g., PsiphonConstants or PsiphonData). Note that the Zirco code
-    	// is customized. When Zirco is first created, it will use the localProxyPort
-    	// and homePages extras to set the proxy preference and open tabs for
-    	// each home page, respectively. When the intent triggers an existing
-    	// Zirco instance (and it's a singleton) the extras are ignored and the
-    	// browser is displayed as-is.
-    	// When a uri is specified, it will open as a new tab. This is
-    	// independent of the home pages.
-    	
-    	intent.putExtra("localProxyPort", PsiphonConstants.HTTP_PROXY_PORT);
+        // This intent displays the Zirco browser.
+        // We use "extras" to communicate Psiphon settings to Zirco, which
+        // is packaged as an independent component (so it's can't access,
+        // e.g., PsiphonConstants or PsiphonData). Note that the Zirco code
+        // is customized. When Zirco is first created, it will use the localProxyPort
+        // and homePages extras to set the proxy preference and open tabs for
+        // each home page, respectively. When the intent triggers an existing
+        // Zirco instance (and it's a singleton) the extras are ignored and the
+        // browser is displayed as-is.
+        // When a uri is specified, it will open as a new tab. This is
+        // independent of the home pages.
+        
+        intent.putExtra("localProxyPort", PsiphonConstants.HTTP_PROXY_PORT);
 
-    	intent.putExtra("homePages", PsiphonData.getPsiphonData().getHomePages());
-    	
-    	intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.putExtra("homePages", PsiphonData.getPsiphonData().getHomePages());
+        
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         context.startActivity(intent);
     }
 }
