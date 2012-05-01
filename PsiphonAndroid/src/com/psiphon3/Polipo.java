561dbe30a4f9ca9a2a541d114000a91fce46ae8c
Rod Hynes <rod-hynes@users.noreply.github.com>
2012-05-01 10:54:14 -0400

    JNI wrapper for Polipo (in progress)

---
+++ PsiphonAndroid/src/com/psiphon3/Polipo.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2012, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.psiphon3;
+
+public class Polipo
+{
+    Thread m_polipoThread;
+
+    public void start()
+    {
+        stop();
+        m_polipoThread = new Thread(
+            new Runnable()
+            {
+                public void run()
+                {
+                    runPolipo();
+                }
+            });
+    }
+    
+    public void stop()
+    {
+        if (m_polipoThread != null)
+        {
+            // TODO: pass in a "stop" object and have polipo gracefully shutdown when the stop flag is set
+            m_polipoThread.stop();
+        }
+        m_polipoThread = null;
+    }
+    
+    private native void runPolipo();
+    
+    static
+    {
+        System.loadLibrary("polipo");
+    }
+}
---
+++ polipo/psiphon-jni.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "jni.h"
+
+int main(int argc, char **argv);
+
+JNIEXPORT void JNICALL  Java_runPolipo_run(JNIEnv* env, jobject obj)
+{
+    // Call Polipo main() with Psiphon command line arguments
+    // TODO: pass in a "stop" object and have polipo gracefully shutdown when the stop flag is set
+    char* args[] =
+    {
+        "proxyPort=8080",
+        "diskCacheRoot=\"\"",
+        "disableLocalInterface=true",
+        "socksParentProxy=127.0.0.1:1080",
+        "logLevel=1"
+    }
+    main(sizeof(args)/sizeof(char*), args);
+}
