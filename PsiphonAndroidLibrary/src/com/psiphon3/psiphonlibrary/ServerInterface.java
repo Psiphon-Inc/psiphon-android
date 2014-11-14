f957f2512180083a22f6537ffc3c9d280b8635fb
Eugene Fryntov <efryntov@users.noreply.github.com>
2014-11-14 13:55:36 -0500

    removed unused import

--- PsiphonAndroidLibrary/src/com/psiphon3/psiphonlibrary/ServerInterface.java
+++ PsiphonAndroidLibrary/src/com/psiphon3/psiphonlibrary/ServerInterface.java
@@ -71,7 +71,6 @@ import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.impl.conn.SystemDefaultDnsResolver;
 import org.apache.http.params.HttpParams;
