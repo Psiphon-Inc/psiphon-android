f594b11c63560ab19422adb7eb804bd03036a89c
Adam Kruger <5760099+adamkruger@users.noreply.github.com>
2016-05-25 23:26:12 -0400

    Fix merge.

--- app/src/main/java/com/psiphon3/psiphonlibrary/TunnelManager.java
+++ app/src/main/java/com/psiphon3/psiphonlibrary/TunnelManager.java
@@ -50,22 +50,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import ca.psiphon.PsiphonTunnel;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import ca.psiphon.PsiphonTunnel;
-
 public class TunnelManager implements PsiphonTunnel.HostService {
 
     public enum NotificationState {
