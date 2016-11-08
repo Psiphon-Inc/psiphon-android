7f51676528f4de24cfa5470e2b75dd05e18c4d47
Adam Kruger <5760099+adamkruger@users.noreply.github.com>
2016-11-08 00:04:03 -0500

    Use MoPub aar.

--- mopub-sdk/src/main/java/com/mopub/common/event/EventRecorder.java
+++
@@ -1,8 +0,0 @@
-package com.mopub.common.event;
-
-/**
- * This interface represents a backend to which MoPub client events are logged.
- */
-public interface EventRecorder {
-    void record(BaseEvent baseEvent);
-}
