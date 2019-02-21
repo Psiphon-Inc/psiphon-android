5a380c5e4da80891e78ae17a754ad056cffa4b37
Adam Scott <a.scott@psiphon.ca>
2019-02-21 14:28:01 -0500

    Added the copyright from YarikSOffice

--- app/src/main/java/com/psiphon3/psiphonlibrary/LocaleManager.java
+++ app/src/main/java/com/psiphon3/psiphonlibrary/LocaleManager.java
@@ -15,6 +15,29 @@ import static android.os.Build.VERSION_CODES.*;
 /**
  * This class is based off YarikSOffice LanguageTest @ https://github.com/YarikSOffice/LanguageTest.
  * Small changes made for Psiphon's use.
+ *
+ * YarikSOffice's Copyright:
+ * MIT License
+ *
+ * Copyright (c) 2017 Yaroslav Berezanskyi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 public class LocaleManager {
     private static final String LANGUAGE_KEY = "language_key";
