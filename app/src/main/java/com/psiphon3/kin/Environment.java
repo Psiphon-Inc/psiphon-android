048d4cc6feb1197742256f839eaa998897ddb64f
Adam Scott <a.scott@psiphon.ca>
2019-09-18 13:46:55 -0400

    Have the Environment create our own Kin.Environment, currently the same but will switch in the future

--- app/src/main/java/com/psiphon3/kin/Environment.java
+++ app/src/main/java/com/psiphon3/kin/Environment.java
@@ -8,13 +8,20 @@ public class Environment {
     public static final Environment PRODUCTION = new Environment(
             "kin.psiphon.io",
             "GAH3A3LKC4XOX6ZUKQWMW3KIMVB2J45NV3OPYBVWADS2RRCYXK7BU3XK",
-            kin.sdk.Environment.PRODUCTION
+            new kin.sdk.Environment(
+                    "https://horizon.kinfederation.com",
+                    "Kin Mainnet ; December 2018"
+            )
     );
     public static final Environment TEST = new Environment(
             "kin-testnet-psiphon.io",
             "GCWUL5BFYTRVMM4WYHABI5C2Y5VZ7VB5N2BKPDMGG3OG7DYN3FBCTVDS",
-            kin.sdk.Environment.TEST
+            new kin.sdk.Environment(
+                    "https://horizon-testnet.kininfrastructure.com/",
+                    "Kin Testnet ; December 2018"
+            )
     );
+
     // TODO: Get real app id
     static final String PSIPHON_APP_ID = "FAKE";
     private final String friendBotServerUrl;
