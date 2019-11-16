1d7486994caaf7a77c4b59a326fb59cc2e60e238
Eugene Fryntov <e.fryntov@psiphon.ca>
2019-11-16 15:58:03 -0500

    - When new subscription authorization for current purchase is stored remove all other persisted authorizations of the same access type.

--- app/src/main/java/com/psiphon3/psiphonlibrary/TunnelManager.java
+++ app/src/main/java/com/psiphon3/psiphonlibrary/TunnelManager.java
@@ -1172,6 +1172,19 @@ public class TunnelManager implements PsiphonTunnel.HostService, MyLog.ILogger {
                                                 return PurchaseVerificationAction.RESTART_AS_NON_SUBSCRIBER;
                                             } else {
                                                 persistPurchaseTokenAndAuthorizationId(purchase.token, authorization.Id());
+                                                // Remove all other authorizations of this type from storage. Psiphon
+                                                // server will only accept one authorization per access type. If there
+                                                // are multiple active authorizations of 'google-subscription' type it is
+                                                // not guaranteed the server will select the one associated with current
+                                                // purchase which may result in client connect-as-subscriber -> server-reject
+                                                // infinite re-connect loop.
+                                                List<Authorization> authorizationsToRemove = new ArrayList<>();
+                                                for (Authorization a : Authorization.geAllPersistedAuthorizations(m_parentService)) {
+                                                    if (a.accessType().equals(authorization.accessType())) {
+                                                        authorizationsToRemove.add(a);
+                                                    }
+                                                }
+                                                Authorization.removeAuthorizations(m_parentService, authorizationsToRemove);
                                                 Authorization.storeAuthorization(getContext(), authorization);
                                                 return PurchaseVerificationAction.RESTART_AS_SUBSCRIBER;
                                             }
@@ -1493,7 +1506,7 @@ public class TunnelManager implements PsiphonTunnel.HostService, MyLog.ILogger {
                     }
                 }
             }
-            // Build a list if not accepted authorizations from the authorizations snapshot
+            // Build a list of not accepted authorizations from the authorizations snapshot
             // by removing all elements of the accepted authorizations list.
             List<Authorization> notAcceptedAuthorizations = m_tunnelConfigAuthorizations;
             notAcceptedAuthorizations.removeAll(acceptedAuthorizations);
