4ddf61df782fb5aaa2ceac3e4c34a028849b27ef
Eugene Fryntov <e.fryntov@psiphon.ca>
2019-12-10 21:16:54 -0500

    MainBase.java: optimize imports

--- app/src/main/java/com/psiphon3/psiphonlibrary/MainBase.java
+++ app/src/main/java/com/psiphon3/psiphonlibrary/MainBase.java
@@ -94,9 +94,6 @@ import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
