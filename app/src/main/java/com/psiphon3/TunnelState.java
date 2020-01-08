a88aba07adeb50520a9b26161af9c45e5795bf4f
Eugene Fryntov <e.fryntov@psiphon.ca>
2020-01-07 19:10:47 -0500

    Ads loading bug fix

--- app/src/main/java/com/psiphon3/TunnelState.java
+++ app/src/main/java/com/psiphon3/TunnelState.java
@@ -116,4 +116,9 @@ public abstract class TunnelState {
     public boolean isUnknown() {
         return status() == Status.UNKNOWN;
     }
+
+    public boolean isStopped() {
+        return status() == Status.STOPPED;
+    }
+
 }
