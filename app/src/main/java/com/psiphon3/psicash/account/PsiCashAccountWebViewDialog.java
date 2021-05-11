903290647c33f057024c4d2d0af0cb25c33df7ec
Eugene Fryntov <e.fryntov@psiphon.ca>
2021-05-10 20:37:55 -0400

    Close all alert dialogs when dismissing PsiCash WebView container

--- app/src/main/java/com/psiphon3/psicash/account/PsiCashAccountWebViewDialog.java
+++ app/src/main/java/com/psiphon3/psicash/account/PsiCashAccountWebViewDialog.java
@@ -19,7 +19,6 @@
 package com.psiphon3.psicash.account;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.net.http.SslError;
@@ -38,16 +37,21 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AlertDialog;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.psiphon3.TunnelState;
 import com.psiphon3.subscription.R;
+
+import java.util.Vector;
 
 import io.reactivex.Flowable;
 import io.reactivex.disposables.Disposable;
 
 public class PsiCashAccountWebViewDialog {
     private final WebView webView;
-    private final Dialog alertDialog;
+    private final Dialog dialog;
+    private final Vector<AlertDialog> alertDialogs = new Vector<>();
     private Disposable tunnelStateDisposable;
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -59,7 +63,7 @@ public class PsiCashAccountWebViewDialog {
         FloatingActionButton floatingActionButton = contentView.findViewById(R.id.close_btn);
 
 
-        alertDialog = new Dialog(context, R.style.Theme_AppCompat_Dialog) {
+        dialog = new Dialog(context, R.style.Theme_AppCompat_Dialog) {
             // Hook up minimal web view navigation
             @Override
             public void onBackPressed() {
@@ -70,8 +74,8 @@ public class PsiCashAccountWebViewDialog {
                 }
             }
         };
-        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        alertDialog.setCancelable(false);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
 
         webView = new WebView(context) {
             @Override
@@ -119,7 +123,7 @@ public class PsiCashAccountWebViewDialog {
             @Override
             public void onCloseWindow(WebView window) {
                 super.onCloseWindow(window);
-                alertDialog.dismiss();
+                close();
             }
         });
 
@@ -128,11 +132,11 @@ public class PsiCashAccountWebViewDialog {
 
         ((LinearLayout) contentView.findViewById(R.id.webview_container_layout)).addView(webView);
 
-        alertDialog.addContentView(contentView,
+        dialog.addContentView(contentView,
                 new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                         ViewGroup.LayoutParams.MATCH_PARENT));
 
-        alertDialog.setOnShowListener(dialog ->
+        dialog.setOnShowListener(dialog ->
                 // Start observing tunnel state and close the dialog if
                 // the tunnel goes offline
                 tunnelStateDisposable = tunnelStateFlowable
@@ -140,12 +144,12 @@ public class PsiCashAccountWebViewDialog {
                         .doOnNext(tunnelState -> {
                             if (!tunnelState.isRunning() ||
                                     !tunnelState.connectionData().isConnected()) {
-                                dialog.dismiss();
+                                close();
                             }
                         })
                         .subscribe());
 
-        alertDialog.setOnDismissListener(dialog -> {
+        dialog.setOnDismissListener(dialog -> {
             webView.stopLoading();
             if (tunnelStateDisposable != null) {
                 tunnelStateDisposable.dispose();
@@ -155,16 +159,15 @@ public class PsiCashAccountWebViewDialog {
 
         floatingActionButton.setOnClickListener(view -> {
             try {
-                new AlertDialog.Builder(context)
+                alertDialogs.add(new AlertDialog.Builder(context)
                         .setIcon(R.drawable.psicash_coin)
                         .setTitle(R.string.psicash_webview_close_title)
                         .setCancelable(true)
                         .setMessage(R.string.psicash_webview_close_message)
                         .setNegativeButton(R.string.lbl_no, (dialog, which) -> {
                         })
-                        .setPositiveButton(R.string.lbl_yes, (dialog, which) ->
-                                alertDialog.dismiss())
-                        .show();
+                        .setPositiveButton(R.string.lbl_yes, (dialog, which) -> close())
+                        .show());
             } catch (RuntimeException ignored) {
             }
         });
@@ -172,23 +175,36 @@ public class PsiCashAccountWebViewDialog {
     }
 
     private void showErrorClosePrompt(Context context) {
-        new AlertDialog.Builder(context)
+        alertDialogs.add(new AlertDialog.Builder(context)
                 .setIcon(R.drawable.psicash_coin)
                 .setTitle(R.string.psicahs_webview_error_alert_title)
                 .setMessage(R.string.psicash_webview_error_alert_message)
-                .setPositiveButton(R.string.Commons_Ok, (dialog, which) -> alertDialog.dismiss())
-                .show();
+                .setPositiveButton(R.string.Commons_Ok, (dialog, which) -> {
+                })
+                .setOnDismissListener(dialog -> close())
+                .show());
     }
 
     public void load(String url) {
         webView.loadUrl(url);
-        alertDialog.show();
+        dialog.show();
 
         // Full screen resize
         WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-        lp.copyFrom(alertDialog.getWindow().getAttributes());
+        lp.copyFrom(dialog.getWindow().getAttributes());
         lp.width = WindowManager.LayoutParams.MATCH_PARENT;
         lp.height = WindowManager.LayoutParams.MATCH_PARENT;
-        alertDialog.getWindow().setAttributes(lp);
+        dialog.getWindow().setAttributes(lp);
+    }
+
+    private void close() {
+        for (AlertDialog alertDialog : alertDialogs) {
+            if (alertDialog.isShowing()) {
+                alertDialog.dismiss();
+            }
+        }
+        if (dialog.isShowing()) {
+            dialog.dismiss();
+        }
     }
 }
