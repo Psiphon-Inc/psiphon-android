3a2a7fd9d4e030e01eb6e64a0639c6d488c29e8a
Eugene Fryntov <e.fryntov@psiphon.ca>
2022-10-27 21:40:07 -0400

    Enable daily, weekly and monthly Speed Boost purchases

--- app/src/main/java/com/psiphon3/psicash/store/SpeedBoostTabFragment.java
+++ app/src/main/java/com/psiphon3/psicash/store/SpeedBoostTabFragment.java
@@ -21,29 +21,27 @@ package com.psiphon3.psicash.store;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Pair;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.gridlayout.widget.GridLayout;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
@@ -58,12 +56,9 @@ import com.psiphon3.psicash.util.UiHelpers;
 import com.psiphon3.subscription.R;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import ca.psiphon.psicashlib.PsiCashLib;
 import io.reactivex.Flowable;
@@ -98,33 +93,33 @@ public class SpeedBoostTabFragment extends Fragment {
         Flowable<TunnelState> tunnelStateFlowable = ((PsiCashStoreActivity) requireActivity()).tunnelStateFlowable();
 
         compositeDisposable.add(Observable.combineLatest(
-                tunnelStateFlowable
-                        .toObservable()
-                        .filter(state -> !state.isUnknown())
-                        .distinctUntilChanged(),
-                psiCashStoreViewModel.states()
-                        .map(psiCashViewState -> {
-                            if (psiCashViewState.purchase() == null) {
-                                return false;
-                            }
-                            Date expiryDate = psiCashViewState.purchase().expiry;
-                            if (expiryDate != null) {
-                                long millisDiff = expiryDate.getTime() - new Date().getTime();
-                                if (millisDiff > 0) {
-                                    // (Re)schedule state refresh after expiry
-                                    handler.removeCallbacksAndMessages(null);
-                                    handler.postDelayed(() -> psiCashStoreViewModel
-                                                    .processIntents(
-                                                            Observable.just(PsiCashStoreIntent.GetPsiCash.create(
-                                                                    tunnelStateFlowable))),
-                                            millisDiff);
-                                    return true;
-                                }
-                            }
-                            return false;
-                        })
-                        .distinctUntilChanged(),
-                ((BiFunction<TunnelState, Boolean, Pair<TunnelState, Boolean>>) Pair::new))
+                        tunnelStateFlowable
+                                .toObservable()
+                                .filter(state -> !state.isUnknown())
+                                .distinctUntilChanged(),
+                        psiCashStoreViewModel.states()
+                                .map(psiCashViewState -> {
+                                    if (psiCashViewState.purchase() == null) {
+                                        return false;
+                                    }
+                                    Date expiryDate = psiCashViewState.purchase().expiry;
+                                    if (expiryDate != null) {
+                                        long millisDiff = expiryDate.getTime() - new Date().getTime();
+                                        if (millisDiff > 0) {
+                                            // (Re)schedule state refresh after expiry
+                                            handler.removeCallbacksAndMessages(null);
+                                            handler.postDelayed(() -> psiCashStoreViewModel
+                                                            .processIntents(
+                                                                    Observable.just(PsiCashStoreIntent.GetPsiCash.create(
+                                                                            tunnelStateFlowable))),
+                                                    millisDiff);
+                                            return true;
+                                        }
+                                    }
+                                    return false;
+                                })
+                                .distinctUntilChanged(),
+                        ((BiFunction<TunnelState, Boolean, Pair<TunnelState, Boolean>>) Pair::new))
                 .distinctUntilChanged()
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnNext(pair -> {
@@ -220,32 +215,56 @@ public class SpeedBoostTabFragment extends Fragment {
             updateUiSpeedBoostPurchaseView(state);
         }
 
-        private final int[] backgrounds = new int[]{
-                R.drawable.speedboost_background_orange,
-                R.drawable.speedboost_background_pink,
-                R.drawable.speedboost_background_purple,
-                R.drawable.speedboost_background_blue,
-                R.drawable.speedboost_background_light_blue,
-                R.drawable.speedboost_background_mint,
-                R.drawable.speedboost_background_orange_2,
-                R.drawable.speedboost_background_yellow,
-                R.drawable.speedboost_background_fluoro_green,
-        };
+        private enum SpeedBoostDistinguisher {
+            SPEEDBOOST_1_HR,
+            SPEEDBOOST_1_DAY,
+            SPEEDBOOST_1_WEEK,
+            SPEEDBOOST_1_MONTH,
+        }
 
-        static public final Map<String, Integer> PSICASH_SKUS_TO_HOURS;
+        private int getSpeedBoostPurchaseDrawableResId(SpeedBoostDistinguisher distinguisher) {
+            switch (distinguisher) {
+                case SPEEDBOOST_1_HR:
+                    return R.drawable.speedboost_background_orange;
+                case SPEEDBOOST_1_DAY:
+                    return R.drawable.speedboost_background_pink;
+                case SPEEDBOOST_1_WEEK:
+                    return R.drawable.speedboost_background_purple;
+                case SPEEDBOOST_1_MONTH:
+                    return R.drawable.speedboost_background_blue;
+                default:
+                    return 0;
+            }
+        }
 
-        static {
-            Map<String, Integer> m = new LinkedHashMap<>();
-            m.put("1hr", 1);
-            m.put("2hr", 2);
-            m.put("3hr", 3);
-            m.put("4hr", 4);
-            m.put("5hr", 5);
-            m.put("6hr", 6);
-            m.put("7hr", 7);
-            m.put("8hr", 8);
-            m.put("9hr", 9);
-            PSICASH_SKUS_TO_HOURS = Collections.unmodifiableMap(m);
+        private String getSpeedBoostDistinguisherValue(SpeedBoostDistinguisher distinguisher) {
+            switch (distinguisher) {
+                case SPEEDBOOST_1_HR:
+                    return "1hr";
+                case SPEEDBOOST_1_DAY:
+                    return "24hr";
+                case SPEEDBOOST_1_WEEK:
+                    return "7day";
+                case SPEEDBOOST_1_MONTH:
+                    return "31day";
+                default:
+                    return null;
+            }
+        }
+
+        private int getDurationStringResId(SpeedBoostDistinguisher distinguisher) {
+            switch (distinguisher) {
+                case SPEEDBOOST_1_HR:
+                    return R.string.speed_boost_product_1_hr;
+                case SPEEDBOOST_1_DAY:
+                    return R.string.speed_boost_product_1_day;
+                case SPEEDBOOST_1_WEEK:
+                    return R.string.speed_boost_product_1_week;
+                case SPEEDBOOST_1_MONTH:
+                    return R.string.speed_boost_product_1_month;
+                default:
+                    return 0;
+            }
         }
 
         private void populateSpeedBoostPurchases(View view, long balance, @NonNull List<PsiCashLib.PurchasePrice> purchasePriceList) {
@@ -256,45 +275,48 @@ public class SpeedBoostTabFragment extends Fragment {
             int orientation = getResources().getConfiguration().orientation;
 
             if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                columnCount = 5;
+                columnCount = 4;
             } else {
-                columnCount = 3;
+                columnCount = 2;
             }
 
-            GridLayout containerLayout = view.findViewById(R.id.purchase_speedboost_grid);
-            containerLayout.removeAllViews();
-            containerLayout.setColumnCount(columnCount);
+            TableLayout containerTableLayout = view.findViewById(R.id.speedboost_purchases_table);
+            containerTableLayout.removeAllViews();
 
-            Drawable buttonDrawable = ResourcesCompat.getDrawable(requireActivity().getResources(), R.drawable.psicash_coin, null);
-            buttonDrawable.setBounds(0,
-                    0,
-                    (int) (buttonDrawable.getIntrinsicWidth() * 0.7),
-                    (int) (buttonDrawable.getIntrinsicHeight() * 0.7));
+            int itemsCount = 0;
+            TableRow tableRow = null;
 
-            for (String distinguisher : PSICASH_SKUS_TO_HOURS.keySet()) {
+            for (SpeedBoostDistinguisher distinguisher : SpeedBoostDistinguisher.values()) {
                 for (PsiCashLib.PurchasePrice price : purchasePriceList) {
-                    if (price == null || !distinguisher.equals(price.distinguisher)) {
+                    if (price == null || !price.distinguisher.equals(getSpeedBoostDistinguisherValue(distinguisher))) {
                         continue;
                     }
 
-                    final String durationString = getDurationString(price.distinguisher, requireActivity().getResources());
-                    if (durationString == null) {
-                        // Skip if we the distinguisher is not in the hardcoded set of PsiCash SKUs
+                    final int durationStringResId = getDurationStringResId(distinguisher);
+                    if (durationStringResId == 0) {
+                        // Skip if the distinguisher is not in the hardcoded set of PsiCash SKUs
                         continue;
                     }
                     LinearLayout speedboostItemLayout = (LinearLayout) requireActivity().getLayoutInflater().inflate(R.layout.speedboost_button_template, null);
                     RelativeLayout relativeLayout = speedboostItemLayout.findViewById(R.id.speedboost_relative_layout);
 
+                    DisplayMetrics metrics = new DisplayMetrics();
+                    requireActivity().getWindow().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+                    float ratio = 248.0f / 185.0f;
+                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, 0);
+                    layoutParams.width = metrics.widthPixels / 3;
+                    layoutParams.height = (int) (layoutParams.width * ratio);
+                    relativeLayout.setLayoutParams(layoutParams);
+
                     final int priceInteger = (int) (Math.floor((long) (price.price / 1e9)));
-                    int drawableResId = getSpeedBoostPurchaseDrawableResId(priceInteger);
+                    int drawableResId = getSpeedBoostPurchaseDrawableResId(distinguisher);
                     relativeLayout.setBackgroundResource(drawableResId);
 
                     TextView durationLabel = speedboostItemLayout.findViewById(R.id.speedboost_purchase_label);
-                    durationLabel.setText(durationString);
+                    durationLabel.setText(durationStringResId);
 
                     Button button = speedboostItemLayout.findViewById(R.id.speedboost_purchase_button);
-
-                    button.setCompoundDrawables(buttonDrawable, null, null, null);
 
                     String priceTag = String.format(Locale.getDefault(), "%d", priceInteger);
                     button.setText(priceTag);
@@ -304,7 +326,7 @@ public class SpeedBoostTabFragment extends Fragment {
                         speedboostItemLayout.setOnClickListener(v -> {
                             String confirmationMessage = String.format(
                                     requireActivity().getString(R.string.confirm_speedboost_purchase_alert),
-                                    durationString,
+                                    requireActivity().getString(durationStringResId),
                                     priceInteger
                             );
                             Flowable<TunnelState> tunnelStateFlowable = ((PsiCashStoreActivity) requireActivity()).tunnelStateFlowable();
@@ -346,33 +368,22 @@ public class SpeedBoostTabFragment extends Fragment {
                                 .create()
                                 .show());
                     }
-                    DisplayMetrics metrics = new DisplayMetrics();
-                    requireActivity().getWindow().getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-                    float ratio = 248.0f / 185.0f;
-                    GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-                    params.width = metrics.widthPixels / columnCount;
-                    params.height = (int) (params.width * ratio);
+                    if (itemsCount % columnCount == 0) {
+                        tableRow = new TableRow(requireActivity());
+                        tableRow.setPadding(0, 16, 0, 16);
+                        containerTableLayout.addView(tableRow);
+                    }
+
+                    TableRow.LayoutParams params = new TableRow.LayoutParams();
+                    params.width = 0;
+                    params.weight = 1f;
+                    params.gravity = Gravity.CENTER;
                     speedboostItemLayout.setLayoutParams(params);
-                    containerLayout.addView(speedboostItemLayout);
+                    tableRow.addView(speedboostItemLayout);
+                    itemsCount++;
                 }
             }
-        }
-
-        private String getDurationString(String distinguisher, Resources resources) {
-            if (TextUtils.isEmpty(distinguisher)) {
-                return null;
-            }
-            Integer duration = PSICASH_SKUS_TO_HOURS.get(distinguisher);
-            if (duration == null) {
-                return null;
-            }
-            return resources.getQuantityString(R.plurals.hours_of_speed_boost, duration, duration);
-        }
-
-        private int getSpeedBoostPurchaseDrawableResId(int priceValue) {
-            int index = ((priceValue / 100) - 1) % backgrounds.length;
-            return backgrounds[index];
         }
 
         private void updateUiPsiCashError(PsiCashStoreViewState state) {
@@ -391,7 +402,7 @@ public class SpeedBoostTabFragment extends Fragment {
                     errorMessage = getString(R.string.unexpected_error_occured_send_feedback_message);
                 }
                 UiHelpers.getSnackbar(errorMessage,
-                        requireActivity().findViewById(R.id.snackbar_anchor_layout))
+                                requireActivity().findViewById(R.id.snackbar_anchor_layout))
                         .show();
             });
         }
