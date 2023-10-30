de7b891ff164dc04713be93192f545fc8526a211
Adam Kruger <a.kruger@psiphon.ca>
2023-10-30 13:29:03 -0400

    Add new tunnel core config parameters

--- app/src/main/java/com/psiphon3/psiphonlibrary/TunnelManager.java
+++ app/src/main/java/com/psiphon3/psiphonlibrary/TunnelManager.java
@@ -1075,6 +1075,8 @@ public class TunnelManager implements PsiphonTunnel.HostService {
                 json.put("UpgradeDownloadURLs", new JSONArray(EmbeddedValues.UPGRADE_URLS_JSON));
 
                 json.put("UpgradeDownloadClientVersionHeader", "x-amz-meta-psiphon-client-version");
+
+                json.put("EnableUpgradeDownload", true);
             }
 
             json.put("MigrateUpgradeDownloadFilename",
@@ -1108,6 +1110,7 @@ public class TunnelManager implements PsiphonTunnel.HostService {
 
             json.put("FeedbackUploadURLs", new JSONArray(EmbeddedValues.FEEDBACK_DIAGNOSTIC_INFO_UPLOAD_URLS_JSON));
             json.put("FeedbackEncryptionPublicKey", EmbeddedValues.FEEDBACK_ENCRYPTION_PUBLIC_KEY);
+            json.put("EnableFeedbackUpload", true);
 
             json.put("AdditionalParameters", EmbeddedValues.ADDITIONAL_PARAMETERS);
 
